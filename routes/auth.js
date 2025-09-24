24693941ead4d393cdd1488789e25dc4d8b133a7
DeivyAyala <107268555+DeivyAyala@users.noreply.github.com>
2025-09-24 17:57:21 -0500

    Cambio a ES Moderno

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post(
     [
         //middelware
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'La contraseña debe de 6 caracteres').not().isEmpty()
+        check('password', 'Ingrese la Contraseña').not().isEmpty()
     ], 
     loginUsuario)
 
